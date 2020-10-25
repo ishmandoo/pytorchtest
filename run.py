3f629cc0227cc0af6d2a4a891871577e36ed67a3
Ben Wiener <ishmandoo@gmail.com>
2020-10-25 12:57:54 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import gym_raas
 print(f"gym version {gym.__version__}")
 
 import tensorflow as tf
-print(f"torch version {tf.__version__}")
+print(f"tensorflow version {tf.__version__}")
 
 import torch
 print(f"torch version {torch.__version__}")
